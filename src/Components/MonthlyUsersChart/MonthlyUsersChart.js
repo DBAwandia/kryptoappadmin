10500ea99e304df38ada29821ecbd19cdcc1c322
ken <kmaina510@gmail.com>
2023-01-28 13:07:43 +0300

    added

--- src/Components/MonthlyUsersChart/MonthlyUsersChart.js
+++ src/Components/MonthlyUsersChart/MonthlyUsersChart.js
@@ -10,8 +10,8 @@ function MonthlyUsersChart() {
         <h1>Monthly Sales overview</h1>
         <div className='MonthlyUsersChart_stat'>
           <ArrowUpward sx={{color: "#2da966"}} className='icon' />
-          <ArrowDownward sx={{color: "#ff4f4f"}} className='icon'/>
-          <p>4% more in 2021</p>
+          {/* <ArrowDownward sx={{color: "#ff4f4f"}} className='icon'/> */}
+          <p>+55% more in 2023</p>
         </div>
         <div className='monthly_chart'>
           <Chart/>
