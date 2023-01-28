7fc58eca2c6e6f5e0b24be38896fe01205433354
ken <kmaina510@gmail.com>
2023-01-28 13:08:23 +0300

    added

--- src/Components/MonthlyUsersChart/MonthlyUsersChart.js
+++ src/Components/MonthlyUsersChart/MonthlyUsersChart.js
@@ -7,7 +7,7 @@ function MonthlyUsersChart() {
   return (
     <div className='MonthlyUsersChart'>
       <div className='MonthlyUsersChart_container'>
-        <h1>Monthly Sales overview</h1>
+        <h1>Monthly Sales overview graph</h1>
         <div className='MonthlyUsersChart_stat'>
           <ArrowUpward sx={{color: "#2da966"}} className='icon' />
           {/* <ArrowDownward sx={{color: "#ff4f4f"}} className='icon'/> */}
